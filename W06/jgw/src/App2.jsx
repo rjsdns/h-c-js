d1a7b9190e6f21088354181dc253c9c0ca82da6b
rjsdns <rjsdns@dongyang.ac.kr>
2026-04-15 12:38:48 +0900

    Add files via upload

---
+++ W06/jgw/src/App2.jsx
@@ -0,0 +1,17 @@
+import Light from "../components/Light";
+import Tab from "../components/tab";
+import SearchForm from "../components/SearchForm";
+import Register from "../components/Register";
+function App2() {
+  return (
+    <>
+      <h1>useState 훅함수 예제</h1>
+      <Light />
+      <Tab />
+      <SearchForm />
+      <Register />
+    </>
+  );
+}
+
+export default App2;
