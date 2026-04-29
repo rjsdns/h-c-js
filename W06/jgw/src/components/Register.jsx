aa0ca2d8563c67e77989371ab5fd23de8df0baa6
rjsdns <rjsdns@dongyang.ac.kr>
2026-04-29 12:05:09 +0900

    Add files via upload

--- W06/jgw/src/components/Register.jsx
+++ W06/jgw/src/components/Register.jsx
@@ -1,30 +1,43 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export default function Register() {
+  let refObj = useRef(0);
+  let inputRef = useRef();
   let [input, setInput] = useState({
     name: "",
     birth: "",
     country: "",
   });
   let onChange = (e) => {
+    refObj.current++;
+    console.log(`호출 횟수 : ${refObj.current}번`);
+
     setInput({
       ...input,
       [e.target.name]: e.target.value,
     });
   };
+  let onSubmit = () => {
+    if (input.name === "") {
+      inputRef.current.focus();
+    }
+  };
   return (
     <>
       <h1>회원 가입</h1>
-
-      이름 : <input name="name" value={input.name} onChange={onChange} />
+      이름 :{" "}
+      <input
+        ref={inputRef}
+        name="name"
+        value={input.name}
+        onChange={onChange}
+      />
       {input.name}
       <br />
-
       생일 :{" "}
       <input name="birth" type="date" value={input.birth} onChange={onChange} />
       {input.birth}
       <br />
-
       국가 :{" "}
       <select name="country" value={input.country} onChange={onChange}>
         <option value=""></option>
@@ -32,6 +45,7 @@ export default function Register() {
         <option value="us">미국</option>
       </select>
       {input.country}
+      <button onClick={onSubmit}>제출</button>
     </>
   );
 }
